811165c9185944595cb658a1e1bb358443b54536
matscap <62808437+matscap@users.noreply.github.com>
2021-04-10 21:30:06 +0100

    Update server.R

--- server.R
+++ server.R
@@ -5,7 +5,6 @@ shinyServer(
   function(input,output,session){
     
     myReactiveDat <- reactive({
-      regiao <- input$Regiao
       
       data <- read.csv("https://opendata.ecdc.europa.eu/covid19/vaccine_tracker/csv/data.csv", na.strings = "", fileEncoding = "UTF-8-BOM")
       #Dados para portugal apenas:
@@ -13,51 +12,65 @@ shinyServer(
         filter(ReportingCountry == "PT")
       
       #Dados por região:
-      if(input$Regiao=="ALENTEJO"){
-        escolha <- dadosapenasportugal %>%
-          filter(Region == "PTCSR01")
-      }else if(regiao == "ALGARVE"){
-        escolha <- dadosapenasportugal %>%
-          filter(Region == "PTCSR02")
-      }else if(regiao == "AZORES"){
-        escolha <- dadosapenasportugal %>%
-          filter(Region == "PTCSR03")
-      }else if(regiao == "CENTRO"){
-        escolha <- dadosapenasportugal %>%
-          filter(Region == "PTCSR04")
-      }else if(regiao == "LISBOA"){
-        escolha <- dadosapenasportugal %>%
-          filter(Region == "PTCSR05")
-      }else if(regiao == "MADEIRA"){
-        escolha <- dadosapenasportugal %>%
-          filter(Region == "PTCSR06")
-      }else if(regiao == "NORTE"){
-        escolha <- dadosapenasportugal %>%
-          filter(Region == "PTCSR07")
-      }else{
-        escolha <- dadosapenasportugal %>%
-          filter(Region == "PT")
+      if(input$opcao=="Por região"){
+        
+          regiao <- input$Regiao
+          
+          if(input$Regiao=="ALENTEJO"){
+            escolha <- dadosapenasportugal %>%
+              filter(Region == "PTCSR01")
+          }else if(regiao == "ALGARVE"){
+            escolha <- dadosapenasportugal %>%
+              filter(Region == "PTCSR02")
+          }else if(regiao == "AZORES"){
+            escolha <- dadosapenasportugal %>%
+              filter(Region == "PTCSR03")
+          }else if(regiao == "CENTRO"){
+            escolha <- dadosapenasportugal %>%
+              filter(Region == "PTCSR04")
+          }else if(regiao == "LISBOA"){
+            escolha <- dadosapenasportugal %>%
+              filter(Region == "PTCSR05")
+          }else if(regiao == "MADEIRA"){
+            escolha <- dadosapenasportugal %>%
+              filter(Region == "PTCSR06")
+          }else if(regiao == "NORTE"){
+            escolha <- dadosapenasportugal %>%
+              filter(Region == "PTCSR07")
+          }else{
+            escolha <- dadosapenasportugal %>%
+              filter(Region == "PT")
+          }
       }
       
       
-      
       #Dados por GRUPOS ETÁRIOS:
-      Age18_24 <- dadosapenasportugal %>%
-        filter(TargetGroup == "Age18_24")
-      Age25_49 <- dadosapenasportugal %>%
-        filter(TargetGroup == "Age25_49")
-      Age50_59 <- dadosapenasportugal %>%
-        filter(TargetGroup == "Age50_59")
-      Age60_69 <- dadosapenasportugal %>%
-        filter(TargetGroup == "Age60_69")
-      Age70_79 <- dadosapenasportugal %>%
-        filter(TargetGroup == "Age70_79")
-      Age80 <- dadosapenasportugal %>%
-        filter(TargetGroup == "Age80+")
-      
+      if(input$opcao=="Por idades"){
+        
+        fetaria <- input$fetaria
+        
+        if(fetaria=="Age18_24"){
+          escolha <- dadosapenasportugal %>%
+            filter(TargetGroup == "Age18_24")
+        }else if(fetaria == "Age25_49"){
+          escolha <- dadosapenasportugal %>%
+            filter(TargetGroup == "Age25_49")
+        }else if(fetaria == "Age50_59"){
+          escolha <- dadosapenasportugal %>%
+            filter(TargetGroup == "Age50_59")
+        }else if(fetaria == "Age60_69"){
+          escolha <- dadosapenasportugal %>%
+            filter(TargetGroup == "Age60_69")
+        }else if(fetaria == "Age70_79"){
+          escolha <- dadosapenasportugal %>%
+            filter(TargetGroup == "Age70_79")
+        }else if(fetaria == "Age80+"){
+          escolha <- dadosapenasportugal %>%
+            filter(TargetGroup == "Age80+")
+        }
+      }
       ##########################################################################
       #histogram de doses por região:
-      
       Data = unlist(escolha["YearWeekISO"])
       Doses=unlist(escolha["FirstDose"])
       
@@ -69,25 +82,15 @@ shinyServer(
     
     output$myPlot <- renderPlot({
       res <- myReactiveDat()
-      #if(input$CHECKBOX)
+      if(input$grafico=="Vacinação semanal")
         ggplot(res$df, aes(x=res$Data, y= res$Doses)) + geom_bar(stat='identity')
-      ##else
-      ##  ggplot(df, aes(x=Data, y=cumsum(Doses))) + geom_line() + geom_point()
-      
-      #########################################################################
-      
-      })
-    output$myPlot2 <- renderPlot({
-      res <- myReactiveDat()
-      
-      #if(input$CHECKBOX)
-      ##ggplot(df, aes(x=Data, y= Doses)) + geom_bar(stat='identity')
-      ##else
+      else if(input$grafico=="Acumulação de vacinas")
         ggplot(res$df, aes(x=res$Data, y=cumsum(res$Doses))) + geom_line() + geom_point()
       
       ##########################################################################
       
-    })
+      })
+   
     
   }
 )
