e5360495dabb2e8fa008a2f6e7d66df1244ee2c8
matscap <62808437+matscap@users.noreply.github.com>
2021-04-20 21:28:14 +0100

    Ler readme antes de usar

--- server.R
+++ server.R
@@ -3,86 +3,40 @@ library(ggplot2)
 shinyServer(
   function(input,output,session){
     
-    myReactiveDat <- reactive({
-      
-      #população por região:
-      
-      #Algarve
-      popalgarve <- 438635
-      popalentejo <- 705.018
-      popnorte <- 3573961
-      poparealisboa <- 2854802
-      popazores <- 241966
-      popmadeira <- 246081
-      popcentro <- 2348162
+    
+    myReactiveDat2 <- reactive({
       
       data <- read.csv("https://opendata.ecdc.europa.eu/covid19/vaccine_tracker/csv/data.csv", na.strings = "", fileEncoding = "UTF-8-BOM")
       #Dados para portugal apenas:
       dadosapenasportugal <- data %>%
         filter(ReportingCountry == "PT")
-      #Dados por região:
-      if(input$opcao=="Por região"){
-        
-          regiao <- input$Regiao
-          
-          if(input$Regiao=="ALENTEJO"){
-            escolha <- dadosapenasportugal %>%
-              filter(Region == "PTCSR01")
-          }else if(regiao == "ALGARVE"){
-            escolha <- dadosapenasportugal %>%
-              filter(Region == "PTCSR02")
-          }else if(regiao == "AZORES"){
-            escolha <- dadosapenasportugal %>%
-              filter(Region == "PTCSR03")
-          }else if(regiao == "CENTRO"){
-            escolha <- dadosapenasportugal %>%
-              filter(Region == "PTCSR04")
-          }else if(regiao == "LISBOA"){
-            escolha <- dadosapenasportugal %>%
-              filter(Region == "PTCSR05")
-          }else if(regiao == "MADEIRA"){
-            escolha <- dadosapenasportugal %>%
-              filter(Region == "PTCSR06")
-          }else if(regiao == "NORTE"){
-            escolha <- dadosapenasportugal %>%
-              filter(Region == "PTCSR07")
-          }else{
-            escolha <- dadosapenasportugal %>%
-              filter(Region == "PT")
-          }
-      }
       
+      dadosapenasportugal <- dadosapenasportugal %>%
+        filter(Region == "PT")
       
-      #Dados por GRUPOS ETÁRIOS:
-      if(input$opcao=="Por idades"){
-        
-        fetaria <- input$fetaria
-        
-        if(fetaria=="Age18_24"){
-          escolha <- dadosapenasportugal %>%
-            filter(TargetGroup == "Age18_24")
-          View(escolha)
-        }else if(fetaria == "Age25_49"){
-          escolha <- dadosapenasportugal %>%
-            filter(TargetGroup == "Age25_49")
-        }else if(fetaria == "Age50_59"){
-          escolha <- dadosapenasportugal %>%
-            filter(TargetGroup == "Age50_59")
-        }else if(fetaria == "Age60_69"){
-          escolha <- dadosapenasportugal %>%
-            filter(TargetGroup == "Age60_69")
-        }else if(fetaria == "Age70_79"){
-          escolha <- dadosapenasportugal %>%
-            filter(TargetGroup == "Age70_79")
-        }else if(fetaria == "Age80+"){
-          escolha <- dadosapenasportugal %>%
-            filter(TargetGroup == "Age80+")
-        }
-      }
+      Age18_24 <- dadosapenasportugal %>%
+        filter(TargetGroup == "Age18_24")
+      
+      Age25_49 <- dadosapenasportugal %>%
+        filter(TargetGroup == "Age25_49")
+
+      Age50_59 <- dadosapenasportugal %>%
+        filter(TargetGroup == "Age50_59")
+
+      Age60_69 <- dadosapenasportugal %>%
+        filter(TargetGroup == "Age60_69")
+
+      Age70_79 <- dadosapenasportugal %>%
+        filter(TargetGroup == "Age70_79")
+
+      Age80 <- dadosapenasportugal %>%
+        filter(TargetGroup == "Age80+")
+      
+
       ##########################################################################
       #histogram de doses por região:
-      Data = unlist(escolha["YearWeekISO"])
-      Doses=unlist(escolha["FirstDose"])
+      Data = unlist(c("Age18_24","Age25_49","Age50_59","Age60_69","Age70_79","Age80"))
+      Doses=unlist(c(sum(Age18_24['FirstDose']),sum(Age25_49['FirstDose']),sum(Age50_59['FirstDose']),sum(Age60_69['FirstDose']),sum(Age70_79['FirstDose']),sum(Age80['FirstDose'])))
       
       df <- data.frame(
         Data,
@@ -90,95 +44,73 @@ shinyServer(
       )
     })
     
-    myReactiveDat2 <- reactive({
+    output$myPlotPais <- renderPlot({
       
-      #população por região:
+      res <- myReactiveDat2()
+      ggplot(res$df, aes(x=res$Data, y= res$Doses)) + 
+          geom_bar(stat='identity')+ 
+          theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
+                axis.title = element_text(face="bold", size=18),
+                title = element_text(size = 20)) + 
+          xlab('Week') +
+          ylab('Doses') 
+
+    })
+    
+    myReactiveDat <- reactive({
       
-      #Algarve
-      popalgarve <- 438635
-      popalentejo <- 705018
-      popnorte <- 3573961
-      poparealisboa <- 2854802
-      popazores <- 241966
-      popmadeira <- 246081
-      popcentro <- 2348162
+      tab <- input$tabs
+      fetaria <- input$fetaria
       
       data <- read.csv("https://opendata.ecdc.europa.eu/covid19/vaccine_tracker/csv/data.csv", na.strings = "", fileEncoding = "UTF-8-BOM")
       #Dados para portugal apenas:
       dadosapenasportugal <- data %>%
         filter(ReportingCountry == "PT")
-        
-        fetaria <- input$fetaria
-        
-        regiao <- input$Regiao
-        #ALENTEJO
-        regiao1 <- dadosapenasportugal %>%
+      
+      
+      
+      if(tab=="Alentejo"){
+        escolha <- dadosapenasportugal %>%
           filter(Region == "PTCSR01")
-        totregiao1 <- sum(regiao1['SecondDose'])
-        #ALGARVE
-        regiao2 <- dadosapenasportugal %>%
+      }else if(tab == "Algarve"){
+        escolha <- dadosapenasportugal %>%
           filter(Region == "PTCSR02")
-        totregiao2 <- sum(regiao2['SecondDose'])
-        #AÇORES
-        regiao3 <- dadosapenasportugal %>%
+      }else if(tab == "Açores"){
+        escolha <- dadosapenasportugal %>%
           filter(Region == "PTCSR03")
-        totregiao3 <- sum(regiao3['SecondDose'])
-        #CENTRO
-        regiao4 <- dadosapenasportugal %>%
+      }else if(tab == "Centro"){
+        escolha <- dadosapenasportugal %>%
           filter(Region == "PTCSR04")
-        totregiao4 <- sum(regiao4['SecondDose'])
-        #LISBOA
-        regiao5 <- dadosapenasportugal %>%
+      }else if(tab == "Lisboa"){
+        escolha <- dadosapenasportugal %>%
           filter(Region == "PTCSR05")
-        totregiao5 <- sum(regiao5['SecondDose'])
-        #MADEIRA
-        regiao6 <- dadosapenasportugal %>%
+      }else if(tab == "Madeira"){
+        escolha <- dadosapenasportugal %>%
           filter(Region == "PTCSR06")
-        totregiao6 <- sum(regiao6['SecondDose'])
-        #NORTE
-        regiao7 <- dadosapenasportugal %>%
+      }else if(tab == "Norte"){
+        escolha <- dadosapenasportugal %>%
           filter(Region == "PTCSR07")
-        totregiao7 <- sum(regiao7['SecondDose'])
-        
-        
-     ##############################################     
-          escolha1 <- dadosapenasportugal %>%
-            filter(TargetGroup == "Age18_24")
-          tot1 <- escolha1['SecondDose']
-          
-          escolha2 <- dadosapenasportugal %>%
-            filter(TargetGroup == "Age25_49")
-          tot2 <- escolha2['SecondDose']
-          
-          escolha3 <- dadosapenasportugal %>%
-            filter(TargetGroup == "Age50_59")
-          tot3 <- escolha3['SecondDose']
-          
-          escolha4 <- dadosapenasportugal %>%
-            filter(TargetGroup == "Age60_69")
-          tot4 <- escolha4['SecondDose']
-          
-          escolha5 <- dadosapenasportugal %>%
-            filter(TargetGroup == "Age70_79")
-          tot5 <- escolha5['SecondDose']
-          
-          escolha6 <- dadosapenasportugal %>%
-            filter(TargetGroup == "Age80+")
-          tot6 <- escolha6['SecondDose']
-          
-          
-          Data  = c("ALENTEJO","ALGRAVE","AÇORES","CENTRO","LISBOA","MADEIRA","NORTE")
-          Doses = c(totregiao1/popalentejo*100,totregiao2/popalgarve*100,totregiao3/popazores*100,totregiao4/popcentro*100,totregiao5/poparealisboa*100,totregiao6/popmadeira*100,totregiao7/popnorte*100)
-
-          df <- data.frame(
-            Data,
-            Doses
-          )
-
+      }else{
+        escolha <- dadosapenasportugal %>%
+          filter(Region == "PT")
+      }
+      
+      
+      Data = unlist(escolha["YearWeekISO"])
+      Doses=unlist(escolha["FirstDose"])
+      
+      df <- data.frame(
+        Data,
+        Doses
+      )
+      
     })
     
+    
     output$myPlot <- renderPlot({
+      
       res <- myReactiveDat()
+      
       if(input$grafico=="Vacinação semanal")
         (ggplot(res$df, aes(x=res$Data, y= res$Doses)) + 
            geom_bar(stat='identity')+ 
@@ -187,114 +119,178 @@ shinyServer(
                  title = element_text(size = 20)) + 
            xlab('Week') +
            ylab('Doses') 
-         )
+        )
       else if(input$grafico=="Acumulação de vacinas")
         ( ggplot(res$df, aes(x=res$Data, y=cumsum(res$Doses))) 
-            + geom_line()  
-            + geom_point() 
-            + theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
+          + geom_line()  
+          + geom_point() 
+          + theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
                   axis.title = element_text(face="bold", size=18),
                   title = element_text(size = 20))  
-            + xlab('Week') 
-            + ylab('Doses') 
+          + xlab('Week') 
+          + ylab('Doses') 
         )
       ##########################################################################
       
-      })
-    output$myPlot2 <- renderPlot({
-      res <- myReactiveDat2()
-     
-      (ggplot(res$df, aes(x=res$Data, y=res$Doses)) 
-        + geom_line() 
-        + geom_bar(stat='identity')
-        + theme(axis.text.x = element_text( vjust = 0.5),
-                axis.title = element_text(face="bold", size=15),
-                title = element_text(size = 20))
-        + xlab('Week') 
-        + ylab('Doses')
-        + labs(title = "Percentagem de pessoas por região que já levaram as duas doses")
-      )##########################################################################
-
     })
-    output$myPlot3 <- renderPlot({
-      #res <- myReactiveDat3()
-      data <- read.csv("https://opendata.ecdc.europa.eu/covid19/vaccine_tracker/csv/data.csv", na.strings = "", fileEncoding = "UTF-8-BOM")
-      #Dados para portugal apenas:
+    output$myPlot2 <- renderPlot({
       
-      dadosapenasportugal <- data %>%
-        filter(ReportingCountry == "PT")
-      dadosapenasportugal2 <- dadosapenasportugal %>%
-        filter(Region != "PT")
+      res <- myReactiveDat()
       
-      dadosapenasportugal2$Region[dadosapenasportugal2$Region=="PTCSR01"] <-"ALENTEJO"
-      dadosapenasportugal2$Region[dadosapenasportugal2$Region=="PTCSR02"] <-"ALGARVE"
-      dadosapenasportugal2$Region[dadosapenasportugal2$Region=="PTCSR03"] <-"AÇORES"
-      dadosapenasportugal2$Region[dadosapenasportugal2$Region=="PTCSR04"] <-"CENTRO"
-      dadosapenasportugal2$Region[dadosapenasportugal2$Region=="PTCSR05"] <-"LISBOA"
-      dadosapenasportugal2$Region[dadosapenasportugal2$Region=="PTCSR06"] <-"MADEIRA"
-      dadosapenasportugal2$Region[dadosapenasportugal2$Region=="PTCSR07"] <-"NORTE"
-      
-      ggplot(dadosapenasportugal2, 
-             aes(x = Region,
-                 y = FirstDose,
-                 label = '')) +
-        geom_point(stat = "identity", 
-                   size = 3) +
-        
-        geom_text(color = 'black',
-                  size = 3,
-                  nudge_x = 0.7) +
-        coord_flip() +
-        
-        theme(legend.position = "none",
-              axis.title = element_text(size = 20,
-                                        face = 'bold'),
-              plot.subtitle = element_text(size = 20,
-                                           face = "bold",
-                                           color = 'red'),
-              axis.text = element_text(size = 10,
-                                       face = "bold"),
-              axis.text.x = element_text(size = 12,
-                                         face = "bold")) +
-        labs(x = 'Regiões',
-             y = 'Doses administradas',
-             title ='Evolução das doses administradas'
+      if(input$grafico1=="Vacinação semanal")
+        (ggplot(res$df, aes(x=res$Data, y= res$Doses)) + 
+           geom_bar(stat='identity')+ 
+           theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
+                 axis.title = element_text(face="bold", size=18),
+                 title = element_text(size = 20)) + 
+           xlab('Week') +
+           ylab('Doses') 
         )
-      #  transition_time(YearWeekISO) +
-      #  ease_aes('cubic-in-out')
+      else if(input$grafico1=="Acumulação de vacinas")
+        ( ggplot(res$df, aes(x=res$Data, y=cumsum(res$Doses))) 
+          + geom_line()  
+          + geom_point() 
+          + theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
+                  axis.title = element_text(face="bold", size=18),
+                  title = element_text(size = 20))  
+          + xlab('Week') 
+          + ylab('Doses') 
+        )
+      ##########################################################################
       
-      #animate(p,
-      #        fps = 3, 
-      #        renderer = gifski_renderer(loop = F),
-      #        end_pause = 10,
-      #        width = 850,
-      #        height = 900)
+    })
+    
+    output$myPlot3 <- renderPlot({
       
-      #anim_save("animations_perCapita_Top35CountriesCovid19ConfirmedTotal.gif")
+      res <- myReactiveDat()
       
-      
+      if(input$grafico2=="Vacinação semanal")
+        (ggplot(res$df, aes(x=res$Data, y= res$Doses)) + 
+           geom_bar(stat='identity')+ 
+           theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
+                 axis.title = element_text(face="bold", size=18),
+                 title = element_text(size = 20)) + 
+           xlab('Week') +
+           ylab('Doses') 
+        )
+      else if(input$grafico2=="Acumulação de vacinas")
+        ( ggplot(res$df, aes(x=res$Data, y=cumsum(res$Doses))) 
+          + geom_line()  
+          + geom_point() 
+          + theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
+                  axis.title = element_text(face="bold", size=18),
+                  title = element_text(size = 20))  
+          + xlab('Week') 
+          + ylab('Doses') 
+        )
       ##########################################################################
       
     })
     
     output$myPlot4 <- renderPlot({
-      data <- read.csv("https://opendata.ecdc.europa.eu/covid19/vaccine_tracker/csv/data.csv", na.strings = "", fileEncoding = "UTF-8-BOM")
-      #Dados para portugal apenas:
       
-      dadosapenasportugal <- data %>%
-        filter(ReportingCountry == "PT")
+      res <- myReactiveDat()
       
-      res <- myReactiveDat2()
+      if(input$grafico3=="Vacinação semanal")
+        (ggplot(res$df, aes(x=res$Data, y= res$Doses)) + 
+           geom_bar(stat='identity')+ 
+           theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
+                 axis.title = element_text(face="bold", size=18),
+                 title = element_text(size = 20)) + 
+           xlab('Week') +
+           ylab('Doses') 
+        )
+      else if(input$grafico3=="Acumulação de vacinas")
+        ( ggplot(res$df, aes(x=res$Data, y=cumsum(res$Doses))) 
+          + geom_line()  
+          + geom_point() 
+          + theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
+                  axis.title = element_text(face="bold", size=18),
+                  title = element_text(size = 20))  
+          + xlab('Week') 
+          + ylab('Doses') 
+        )
+      ##########################################################################
       
-      (ggplot(dadosapenasportugal, aes(x=Vaccine, y= FirstDose)) + 
-          geom_bar(stat='identity')+ 
-          theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
-                axis.title = element_text(face="bold", size=18),
-                title = element_text(size = 20)) + 
-          xlab('Week') +
-          ylab('Doses') 
-      )
+    })
+    
+    output$myPlot5 <- renderPlot({
       
+      res <- myReactiveDat()
+      
+      if(input$grafico4=="Vacinação semanal")
+        (ggplot(res$df, aes(x=res$Data, y= res$Doses)) + 
+           geom_bar(stat='identity')+ 
+           theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
+                 axis.title = element_text(face="bold", size=18),
+                 title = element_text(size = 20)) + 
+           xlab('Week') +
+           ylab('Doses') 
+        )
+      else if(input$grafico4=="Acumulação de vacinas")
+        ( ggplot(res$df, aes(x=res$Data, y=cumsum(res$Doses))) 
+          + geom_line()  
+          + geom_point() 
+          + theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
+                  axis.title = element_text(face="bold", size=18),
+                  title = element_text(size = 20))  
+          + xlab('Week') 
+          + ylab('Doses') 
+        )
+      ##########################################################################
+      
+    })
+    
+    output$myPlot6 <- renderPlot({
+      
+      res <- myReactiveDat()
+      
+      if(input$grafico5=="Vacinação semanal")
+        (ggplot(res$df, aes(x=res$Data, y= res$Doses)) + 
+           geom_bar(stat='identity')+ 
+           theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
+                 axis.title = element_text(face="bold", size=18),
+                 title = element_text(size = 20)) + 
+           xlab('Week') +
+           ylab('Doses') 
+        )
+      else if(input$grafico5=="Acumulação de vacinas")
+        ( ggplot(res$df, aes(x=res$Data, y=cumsum(res$Doses))) 
+          + geom_line()  
+          + geom_point() 
+          + theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
+                  axis.title = element_text(face="bold", size=18),
+                  title = element_text(size = 20))  
+          + xlab('Week') 
+          + ylab('Doses') 
+        )
+      ##########################################################################
+      
+    })
+    
+    output$myPlot7 <- renderPlot({
+      
+      res <- myReactiveDat()
+      
+      if(input$grafico6=="Vacinação semanal")
+        (ggplot(res$df, aes(x=res$Data, y= res$Doses)) + 
+           geom_bar(stat='identity')+ 
+           theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
+                 axis.title = element_text(face="bold", size=18),
+                 title = element_text(size = 20)) + 
+           xlab('Week') +
+           ylab('Doses') 
+        )
+      else if(input$grafico6=="Acumulação de vacinas")
+        ( ggplot(res$df, aes(x=res$Data, y=cumsum(res$Doses))) 
+          + geom_line()  
+          + geom_point() 
+          + theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
+                  axis.title = element_text(face="bold", size=18),
+                  title = element_text(size = 20))  
+          + xlab('Week') 
+          + ylab('Doses') 
+        )
       ##########################################################################
       
     })
