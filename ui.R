c007d30de201f575c51eb87ec017a7648c1481ca
matscap <62808437+matscap@users.noreply.github.com>
2021-04-22 21:42:55 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -10,8 +10,22 @@ shinyServer(
   fluidPage(theme = bs_theme(version = 4, bootswatch = "united"),
             
             navbarPage("",
-              tabPanel("Portugal", fluid = TRUE,
-                       plotOutput("myPlotPais")%>% withSpinner(color="#cccccc"))
+              tabPanel("Portugal", fluid = TRUE,pageWithSidebar(
+                      headerPanel(""),
+                      
+                      sidebarPanel(
+                        radioButtons("dist", "Gráfico:",
+                                     c("Por idades" = "norm",
+                                       "Por vacinas" = "unif"))
+                      ),
+                      
+                      mainPanel(
+                        tags$div(style="row", id="yo",checked=NA,
+                                 tags$div(style="col-12",checked=NA,plotOutput("myPlotPais")%>% withSpinner(color="#cccccc")))
+                        )
+                        #plotOutput("myPlot")
+                      )
+                    )   
                            ,
               tabPanel("Regiões", fluid = TRUE,
                        tabsetPanel(id="tabs",
@@ -20,12 +34,17 @@ shinyServer(
                                               headerPanel(""),
                                               
                                               sidebarPanel(
-                                                       selectInput("grafico","Selecione o tipo de gráfico:",c("Vacinação semanal","Acumulação de vacinas"))
-                                              ),
+                                                       selectInput("grafico","Selecione o tipo de gráfico:",c("Vacinação semanal","Acumulação de vacinas")),
+                                                       radioButtons("radiovac1", "Gráfico:",
+                                                                    c("Moderna" = "mod",
+                                                                      "AstraZeneca" = "az",
+                                                                      "Pfizer"="pf"))
+                                                  
+                                             ),
                                               
                                               mainPanel(
-                                                tags$div(style="row", id="yo",checked=NA,
-                                                         tags$div(style="col-12",checked=NA,plotOutput("myPlot")%>% withSpinner(color="#cccccc"))
+                                                tags$div(style="row",checked=NA,
+                                                         tags$div(style="col-12",checked=NA,plotOutput("myPlotB")%>% withSpinner(color="#cccccc"))
                                                 )
                                                 #plotOutput("myPlot")
                                               )
@@ -37,7 +56,11 @@ shinyServer(
                                               headerPanel(""),
                                               
                                               sidebarPanel(
-                                                selectInput("grafico1","Selecione o tipo de gráfico:",c("Vacinação semanal","Acumulação de vacinas"))
+                                                selectInput("grafico1","Selecione o tipo de gráfico:",c("Vacinação semanal","Acumulação de vacinas")),
+                                                radioButtons("radiovac2", "Gráfico:",
+                                                             c("Moderna" = "mod",
+                                                               "AstraZeneca" = "az",
+                                                               "Pfizer"="pf"))
                                               ),
                                               
                                               mainPanel(
@@ -53,7 +76,11 @@ shinyServer(
                                               headerPanel(""),
                                               
                                               sidebarPanel(
-                                                selectInput("grafico2","Selecione o tipo de gráfico:",c("Vacinação semanal","Acumulação de vacinas"))
+                                                selectInput("grafico2","Selecione o tipo de gráfico:",c("Vacinação semanal","Acumulação de vacinas")),
+                                                radioButtons("radiovac3", "Gráfico:",
+                                                             c("Moderna" = "mod",
+                                                               "AstraZeneca" = "az",
+                                                               "Pfizer"="pf"))
                                               ),
                                               
                                               mainPanel(
@@ -69,7 +96,11 @@ shinyServer(
                                               headerPanel(""),
                                               
                                               sidebarPanel(
-                                                selectInput("grafico3","Selecione o tipo de gráfico:",c("Vacinação semanal","Acumulação de vacinas"))
+                                                selectInput("grafico3","Selecione o tipo de gráfico:",c("Vacinação semanal","Acumulação de vacinas")),
+                                                radioButtons("radiovac4", "Gráfico:",
+                                                             c("Moderna" = "mod",
+                                                               "AstraZeneca" = "az",
+                                                               "Pfizer"="pf"))
                                               ),
                                               
                                               mainPanel(
@@ -85,7 +116,10 @@ shinyServer(
                                               headerPanel(""),
                                               
                                               sidebarPanel(
-                                                selectInput("grafico4","Selecione o tipo de gráfico:",c("Vacinação semanal","Acumulação de vacinas"))
+                                                selectInput("grafico4","Selecione o tipo de gráfico:",c("Vacinação semanal","Acumulação de vacinas")),
+                                                radioButtons("radiovac5", "Gráfico:",
+                                                             c("AstraZeneca" = "az",
+                                                               "Pfizer"="pf"))
                                               ),
                                               
                                               mainPanel(
@@ -101,7 +135,10 @@ shinyServer(
                                               headerPanel(""),
                                               
                                               sidebarPanel(
-                                                selectInput("grafico5","Selecione o tipo de gráfico:",c("Vacinação semanal","Acumulação de vacinas"))
+                                                selectInput("grafico5","Selecione o tipo de gráfico:",c("Vacinação semanal","Acumulação de vacinas")),
+                                                radioButtons("radiovac6", "Gráfico:",
+                                                             c("AstraZeneca" = "az",
+                                                               "Pfizer"="pf"))
                                               ),
                                               
                                               mainPanel(
@@ -117,7 +154,11 @@ shinyServer(
                                               headerPanel(""),
                                               
                                               sidebarPanel(
-                                                selectInput("grafico6","Selecione o tipo de gráfico:",c("Vacinação semanal","Acumulação de vacinas"))
+                                                selectInput("grafico6","Selecione o tipo de gráfico:",c("Vacinação semanal","Acumulação de vacinas")),
+                                                radioButtons("radiovac7", "Gráfico:",
+                                                             c("Moderna" = "mod",
+                                                               "AstraZeneca" = "az",
+                                                               "Pfizer"="pf"))
                                               ),
                                               
                                               mainPanel(
