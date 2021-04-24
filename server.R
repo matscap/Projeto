020ba8469f839d7d1c725c43af1e33765b747d24
matscap <62808437+matscap@users.noreply.github.com>
2021-04-24 20:07:52 +0100

    Update server.R

--- server.R
+++ server.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(ggplot2)
+library(tidyr)
 shinyServer(
   function(input,output,session){
     
@@ -47,69 +48,118 @@ shinyServer(
     })
     
     
-    myReactiveDat2 <- reactive({
+    myReactiveDataPais <- reactive({
       
+      data <- read.csv("https://opendata.ecdc.europa.eu/covid19/vaccine_tracker/csv/data.csv", na.strings = "", fileEncoding = "UTF-8-BOM")
       #Dados para portugal apenas:
-      dadosapenasportugal <- loadedData() %>%
+      dadosapenasportugal <- data %>%
         filter(ReportingCountry == "PT")
       
       dadosapenasportugal <- dadosapenasportugal %>%
         filter(Region == "PT")
-
       
       Age18_24 <- dadosapenasportugal %>%
         filter(TargetGroup == "Age18_24")
       
       Age25_49 <- dadosapenasportugal %>%
         filter(TargetGroup == "Age25_49")
-
+      
       Age50_59 <- dadosapenasportugal %>%
         filter(TargetGroup == "Age50_59")
-
+      
       Age60_69 <- dadosapenasportugal %>%
         filter(TargetGroup == "Age60_69")
-
+      
       Age70_79 <- dadosapenasportugal %>%
         filter(TargetGroup == "Age70_79")
-
+      
       Age80 <- dadosapenasportugal %>%
         filter(TargetGroup == "Age80+")
       
-
+      
+      ### RAFA ###################################################################
+      #Ideia para ver o total de gente vacinada (corretamente)
+      #Temos de filtrar a df dadosapenasportugal para apenas uma linha de cada week com os dados de ALL
+      ## Region == "PT"
+      ## TargetGroup == "All"
+      totalPopulacao = 10295909
+      totalGenteVacinada <- dadosapenasportugal %>%
+        filter(TargetGroup == "ALL") %>%
+        filter(grepl("PT$",Region))
+      
+      #tpv -> total de pessoas vacinadas pela vacina _ ###
+      tpv_MOD <- totalGenteVacinada %>%
+        filter(Vaccine == "MOD")
+      tpv_COM <- totalGenteVacinada %>%
+        filter(Vaccine == "COM")
+      tpv_AZ <- totalGenteVacinada %>%
+        filter(Vaccine == "AZ")
+      
+      TotalVacinados = unlist(c(sum(tpv_MOD["SecondDose"])) + c(sum(tpv_COM["SecondDose"])) + c(sum(tpv_AZ["SecondDose"])))
+      Percentagem_tpv = (TotalVacinados/totalPopulacao)*100
+      Percentagem_tpNv = ((totalPopulacao-TotalVacinados)/totalPopulacao)*100
       ##########################################################################
       #histogram de doses por região:
-      Data = unlist(c("Age18_24","Age25_49","Age50_59","Age60_69","Age70_79","Age80"))
+      Idades = unlist(c("18-24","25-49","50-59","60-69","70-79", "80+"))
       Doses=unlist(c(sum(Age18_24['FirstDose']),sum(Age25_49['FirstDose']),sum(Age50_59['FirstDose']),sum(Age60_69['FirstDose']),sum(Age70_79['FirstDose']),sum(Age80['FirstDose'])))
       
-      df <- data.frame(
-        Data,
+      dfpais <- data.frame(
+        Idades,
         Doses
       )
     })
-    
+    c5 = c("18-24","25-49","50-59","60-69","70-79", "80+")
     output$myPlotPais <- renderPlot({
-      opcao <- input$dist
-      
+      res <- myReactiveDataPais()
       #vac <- Vacinas()
-      if(opcao=="norm"){
-        res <- myReactiveDat2()
-        ggplot(res$df, aes(x=res$Data, y= res$Doses)) + 
-            geom_bar(stat='identity')+ 
-            theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
-                  axis.title = element_text(face="bold", size=18),
-                  title = element_text(size = 20)) + 
-            xlab('Week') +
-            ylab('Doses') 
-      }else if(opcao=="unif"){
-        res <- Vacinas()
-        ggplot(res$df, aes(x=res$Nomes, y= res$Totais)) + 
-          geom_bar(stat='identity')+ 
-          theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
-                axis.title = element_text(face="bold", size=18),
-                title = element_text(size = 20)) + 
-          xlab('Vacinas') +
-          ylab('Doses') 
-      }
+        ggplot(data = res$dfpais, aes(y=as.factor(res$Idades), x= as.factor(res$Doses), fill = c5)) + 
+          geom_bar(stat='identity', orientation = "y")+
+          scale_fill_manual(values = c("18-24"="#D8BFD8",
+                                       "25-49"="#B0E0E6",
+                                       "50-59"="#FFDAB9",
+                                       "60-69"="#FA8072",
+                                       "70-79"="#98FB98",
+                                       "80+"="#FFFACD")) +
+          theme(
+            legend.position = "none",
+            axis.text.x = element_text(angle = 45, vjust = 0.5),
+            axis.title = element_text(face="bold", size=18),
+            title = element_text(size = 20)
+          ) + 
+          labs(title = 'Por o titulo desta cena') +
+          xlab('Nº de pessoas Vacinadas') +
+          ylab('') 
+      
+    })
+    c6 = c("AZ","MOD","COM")
+    output$myPlotPais2 <- renderPlot({
+      
+      res <- Vacinas()
+      ggplot(res$df, aes(x=res$Nomes, y= res$Totais, fill = c6)) + 
+        geom_bar(stat='identity')+ 
+        scale_fill_manual(values = c("AZ"="#D8BFD8",
+                                     "MOD"="#B0E0E6",
+                                     "COM"="#FFDAB9")) +
+        theme(
+          legend.position = "none",
+          axis.text.x = element_text(angle = 45, vjust = 0.5),
+          axis.title = element_text(face="bold", size=18),
+          title = element_text(size = 20)
+        ) + 
+        theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
+              axis.title = element_text(face="bold", size=18),
+              title = element_text(size = 20)) + 
+        xlab('Vacinas') +
+        ylab('Doses') 
+    })
+    
+    output$myPie <- renderPlot({
+      
+      slices <- c(6,94)
+      #slices = c(Percentagem_tpv, Percentagem_tpNv)
+      nomenclatura <- c("Vacinados","Nao Vacinados")
+      #ggplot() + 
+      pie(slices, labels = nomenclatura, main="qq coisa")
     })
     
     myReactiveDat <- reactive({
@@ -152,7 +202,7 @@ shinyServer(
       }else if(tab == "Açores"){
         escolha <- dadosapenasportugal %>%
           filter(Region == "PTCSR03")
-        tipo <- input$radiovac3
+        tipo <- input$radiovac5
         if(tipo=="mod"){
           marca <-escolha %>%
             filter(Vaccine == "MOD")
@@ -180,7 +230,7 @@ shinyServer(
       }else if(tab == "Lisboa"){
         escolha <- dadosapenasportugal %>%
           filter(Region == "PTCSR05")
-        tipo <- input$radiovac5
+        tipo <- input$radiovac3
         
         if(tipo=="mod"){
           marca <-escolha %>%
