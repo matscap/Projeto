dc9dd989bb4b96e818375cbfcf5269f6b169a38f
matscap <62808437+matscap@users.noreply.github.com>
2021-06-07 21:50:44 +0100

    Update server.R

--- server.R
+++ server.R
@@ -1,96 +1,468 @@
 library(dplyr)
 library(ggplot2)
+library(tidyr)
+library(reshape)
+library(gganimate)
+
+last_update <- Sys.Date()
+
+# Vamos usar como data inicial, a quinta-feira da semana de chegada do primeiro lote de vacinas a Portugal
+date <- as.Date("24/12/2021", format = "%d/%m/%y")
+
+
 shinyServer(
-  
   function(input,output,session){
     
-    myReactiveDat <- reactive({
-      
+    loadedData <- reactiveVal()
+    loadedData2 <- reactiveVal()
+    
+    loaded_Alentejo <- reactiveVal()
+    loaded_Algarve <- reactiveVal()
+    loaded_Acores <- reactiveVal()
+    loaded_Centro <- reactiveVal()
+    loaded_Lisboa <- reactiveVal()
+    loaded_Madeira <- reactiveVal()
+    loaded_Norte <- reactiveVal()
+    loaded_PT <- reactiveVal()
+    
+    if(last_update + 7 >= Sys.Date()) {
+      last_update <- Sys.Date()
       data <- read.csv("https://opendata.ecdc.europa.eu/covid19/vaccine_tracker/csv/data.csv", na.strings = "", fileEncoding = "UTF-8-BOM")
-      #Dados para portugal apenas:
-      dadosapenasportugal <- data %>%
+      write.csv(data, "Data/data.csv")
+    }
+    
+    observe({
+      loadedData(read.csv("Data/data.csv", na.strings = "", fileEncoding = "UTF-8-BOM")) 
+      dadosapenasportugal <- loadedData() %>%
         filter(ReportingCountry == "PT")
+      loadedData2(dadosapenasportugal)
       
-      #Dados por região:
-      if(input$opcao=="Por região"){
-        
-          regiao <- input$Regiao
-          
-          if(input$Regiao=="ALENTEJO"){
-            escolha <- dadosapenasportugal %>%
-              filter(Region == "PTCSR01")
-          }else if(regiao == "ALGARVE"){
-            escolha <- dadosapenasportugal %>%
-              filter(Region == "PTCSR02")
-          }else if(regiao == "AZORES"){
-            escolha <- dadosapenasportugal %>%
-              filter(Region == "PTCSR03")
-          }else if(regiao == "CENTRO"){
-            escolha <- dadosapenasportugal %>%
-              filter(Region == "PTCSR04")
-          }else if(regiao == "LISBOA"){
-            escolha <- dadosapenasportugal %>%
-              filter(Region == "PTCSR05")
-          }else if(regiao == "MADEIRA"){
-            escolha <- dadosapenasportugal %>%
-              filter(Region == "PTCSR06")
-          }else if(regiao == "NORTE"){
-            escolha <- dadosapenasportugal %>%
-              filter(Region == "PTCSR07")
-          }else{
-            escolha <- dadosapenasportugal %>%
-              filter(Region == "PT")
-          }
-      }
+      Alentejo <- loadedData2() %>%
+        filter(Region == "PTCSR01")
+      loaded_Alentejo(Alentejo)
       
+      Algarve <- loadedData2() %>%
+        filter(Region == "PTCSR02")
+      loaded_Algarve(Algarve)
       
-      #Dados por GRUPOS ETÁRIOS:
-      if(input$opcao=="Por idades"){
-        
-        fetaria <- input$fetaria
-        
-        if(fetaria=="Age18_24"){
-          escolha <- dadosapenasportugal %>%
-            filter(TargetGroup == "Age18_24")
-        }else if(fetaria == "Age25_49"){
-          escolha <- dadosapenasportugal %>%
-            filter(TargetGroup == "Age25_49")
-        }else if(fetaria == "Age50_59"){
-          escolha <- dadosapenasportugal %>%
-            filter(TargetGroup == "Age50_59")
-        }else if(fetaria == "Age60_69"){
-          escolha <- dadosapenasportugal %>%
-            filter(TargetGroup == "Age60_69")
-        }else if(fetaria == "Age70_79"){
-          escolha <- dadosapenasportugal %>%
-            filter(TargetGroup == "Age70_79")
-        }else if(fetaria == "Age80+"){
-          escolha <- dadosapenasportugal %>%
-            filter(TargetGroup == "Age80+")
-        }
-      }
-      ##########################################################################
-      #histogram de doses por região:
-      Data = unlist(escolha["YearWeekISO"])
-      Doses=unlist(escolha["FirstDose"])
+      Açores <- loadedData2() %>%
+        filter(Region == "PTCSR03")
+      loaded_Acores(Açores)
       
-      df <- data.frame(
-        Data,
-        Doses
-      )
+      Centro <- loadedData2() %>%
+        filter(Region == "PTCSR04")
+      loaded_Centro(Centro)
+      
+      Lisboa <- loadedData2() %>%
+        filter(Region == "PTCSR05")
+      loaded_Lisboa(Lisboa)
+      
+      Madeira <- loadedData2() %>%
+        filter(Region == "PTCSR06")
+      loaded_Madeira(Madeira)
+      
+      Norte <- loadedData2() %>%
+        filter(Region == "PTCSR07")
+      loaded_Norte(Norte)
+      
+      PT <- loadedData2() %>%
+        filter(Region == "PT")
+      loaded_PT(PT)
+      
     })
     
-    output$myPlot <- renderPlot({
-      res <- myReactiveDat()
-      if(input$grafico=="Vacinação semanal")
-        ggplot(res$df, aes(x=res$Data, y= res$Doses)) + geom_bar(stat='identity')
-      else if(input$grafico=="Acumulação de vacinas")
-        ggplot(res$df, aes(x=res$Data, y=cumsum(res$Doses))) + geom_line() + geom_point()
-      
-      ##########################################################################
-      
-      })
-   
     
-  }
-)
+    observeEvent(input$ajuda, {
+      showModal(modalDialog(
+        title = "Ajuda",
+        "Uma pessoa vacinada é aquela que completou o plano de vacinação",
+        easyClose = TRUE
+      ))
+    })
+    
+    source('Portugal/Dados.R', local = TRUE)$value
+    source('Portugal/Plots.R', local = TRUE)$value
+    source('Regiões/DadosReg.R', local = TRUE)$value
+    source('Regiões/PlotsReg.R', local = TRUE)$value
+    
+    
+    
+    output$myEvolucao <- renderPlot({
+      
+      Alentejo <- loaded_Alentejo()
+      
+      Alentejo$FirstDoseRefused <- NULL
+      Alentejo$UnknownDose <- NULL
+      Alentejo$NumberDosesReceived <- NULL
+      Alentejo$Population <- NULL
+      Alentejo$ReportingCountry <- NULL
+      Alentejo$TargetGroup <- NULL
+      Alentejo$Denominator <- NULL
+      Alentejo$Region <- NULL
+      Alentejo$X <- NULL
+      
+      datas = unique(Alentejo["YearWeekISO"])
+      
+      aux=Alentejo[1,2]+Alentejo[1,3]
+      dosestotaisAlentejo = c()
+      
+      for(i in 2:nrow(Alentejo)) {
+        # for-loop over rows
+        if(Alentejo[i,1]==Alentejo[i-1,1]){
+          aux = aux + Alentejo[i,2]+Alentejo[i,3]
+          
+          if(i==nrow(Alentejo)){
+            dosestotaisAlentejo <- c(dosestotaisAlentejo, aux)
+          }
+        }else{
+          dosestotaisAlentejo <- c(dosestotaisAlentejo, aux)
+          aux = Alentejo[i,2]+Alentejo[i,3]
+          if(i==nrow(Alentejo)){
+            dosestotaisAlentejo <- c(dosestotaisAlentejo, aux)
+          }
+        }
+        
+      }
+      
+      dfAlentejo <- data.frame(
+        datas,
+        dosestotaisAlentejo
+      )
+      
+      #######################################
+      Algarve <- loaded_Algarve()
+      
+      Algarve$FirstDoseRefused <- NULL
+      Algarve$UnknownDose <- NULL
+      Algarve$NumberDosesReceived <- NULL
+      Algarve$Population <- NULL
+      Algarve$ReportingCountry <- NULL
+      Algarve$TargetGroup <- NULL
+      Algarve$Denominator <- NULL
+      Algarve$Region <- NULL
+      Algarve$X <- NULL
+      datas = unique(Algarve["YearWeekISO"])
+      
+      aux=Algarve[1,2]+Algarve[1,3]
+      
+      dosestotaisAlgarve = c()
+      
+      for(i in 2:nrow(Algarve)) {
+        # for-loop over rows
+        if(Algarve[i,1]==Algarve[i-1,1]){
+          aux = aux + Algarve[i,2]+Algarve[i,3]
+          
+          if(i==nrow(Algarve)){
+            dosestotaisAlgarve <- c(dosestotaisAlgarve, aux)
+          }
+        }else{
+          dosestotaisAlgarve <- c(dosestotaisAlgarve, aux)
+          aux = Algarve[i,2]+Algarve[i,3]
+          if(i==nrow(Algarve)){
+            dosestotaisAlgarve <- c(dosestotaisAlgarve, aux)
+          }
+        }
+        
+      }
+      
+      dfAlgarve <- data.frame(
+        datas,
+        dosestotaisAlgarve
+      )
+      
+      #######################################
+      Açores <- loaded_Acores()
+      
+      Açores$FirstDoseRefused <- NULL
+      Açores$UnknownDose <- NULL
+      Açores$NumberDosesReceived <- NULL
+      Açores$Population <- NULL
+      Açores$ReportingCountry <- NULL
+      Açores$TargetGroup <- NULL
+      Açores$Denominator <- NULL
+      Açores$Region <- NULL
+      Açores$X <- NULL
+      datasAçores = unique(Açores["YearWeekISO"])
+      
+      aux=Açores[1,2]+Açores[1,3]
+      
+      dosestotaisAçores = c()
+      
+      for(i in 2:nrow(Açores)) {
+        # for-loop over rows
+        if(Açores[i,1]==Açores[i-1,1]){
+          aux = aux + Açores[i,2]+Açores[i,3]
+          
+          if(i==nrow(Açores)){
+            dosestotaisAçores <- c(dosestotaisAçores, aux)
+          }
+        }else{
+          dosestotaisAçores <- c(dosestotaisAçores, aux)
+          aux = Açores[i,2]+Açores[i,3]
+          if(i==nrow(Açores)){
+            dosestotaisAçores <- c(dosestotaisAçores, aux)
+          }
+        }
+        
+      }
+      
+      dfAçores <- data.frame(
+        datasAçores,
+        dosestotaisAçores
+      )
+      
+      #######################################
+      Centro <- loaded_Centro()
+      
+      Centro$FirstDoseRefused <- NULL
+      Centro$UnknownDose <- NULL
+      Centro$NumberDosesReceived <- NULL
+      Centro$Population <- NULL
+      Centro$ReportingCountry <- NULL
+      Centro$TargetGroup <- NULL
+      Centro$Denominator <- NULL
+      Centro$Region <- NULL
+      Centro$X <- NULL
+      datasCentro = unique(Centro["YearWeekISO"])
+      
+      aux=Centro[1,2]+Centro[1,3]
+      
+      dosestotaisCentro = c()
+      
+      for(i in 2:nrow(Centro)) {
+        # for-loop over rows
+        if(Centro[i,1]==Centro[i-1,1]){
+          aux = aux + Centro[i,2]+Centro[i,3]
+          
+          if(i==nrow(Centro)){
+            dosestotaisCentro <- c(dosestotaisCentro, aux)
+          }
+        }else{
+          dosestotaisCentro <- c(dosestotaisCentro, aux)
+          aux = Centro[i,2]+Centro[i,3]
+          if(i==nrow(Centro)){
+            dosestotaisCentro <- c(dosestotaisCentro, aux)
+          }
+        }
+        
+      }
+      
+      dfCentro <- data.frame(
+        datasCentro,
+        dosestotaisCentro
+      )
+      
+      #######################################
+      Lisboa <- loaded_Lisboa()
+      
+      Lisboa$FirstDoseRefused <- NULL
+      Lisboa$UnknownDose <- NULL
+      Lisboa$NumberDosesReceived <- NULL
+      Lisboa$Population <- NULL
+      Lisboa$ReportingCountry <- NULL
+      Lisboa$TargetGroup <- NULL
+      Lisboa$Denominator <- NULL
+      Lisboa$Region <- NULL
+      Lisboa$X <- NULL
+      datasLisboa = unique(Lisboa["YearWeekISO"])
+      
+      aux=Lisboa[1,2]+Lisboa[1,3]
+      
+      dosestotaisLisboa = c()
+      
+      for(i in 2:nrow(Lisboa)) {
+        # for-loop over rows
+        if(Lisboa[i,1]==Lisboa[i-1,1]){
+          aux = aux + Lisboa[i,2]+Lisboa[i,3]
+          
+          if(i==nrow(Lisboa)){
+            dosestotaisLisboa <- c(dosestotaisLisboa, aux)
+          }
+        }else{
+          dosestotaisLisboa <- c(dosestotaisLisboa, aux)
+          aux = Lisboa[i,2]+Lisboa[i,3]
+          if(i==nrow(Lisboa)){
+            dosestotaisLisboa <- c(dosestotaisLisboa, aux)
+          }
+        }
+        
+      }
+      
+      dfLisboa <- data.frame(
+        datasLisboa,
+        dosestotaisLisboa
+      )
+      
+      #######################################
+      Madeira <- loaded_Madeira()
+      
+      Madeira$FirstDoseRefused <- NULL
+      Madeira$UnknownDose <- NULL
+      Madeira$NumberDosesReceived <- NULL
+      Madeira$Population <- NULL
+      Madeira$ReportingCountry <- NULL
+      Madeira$TargetGroup <- NULL
+      Madeira$Denominator <- NULL
+      Madeira$Region <- NULL
+      Madeira$X <- NULL
+      datasMadeira = unique(Madeira["YearWeekISO"])
+      
+      aux=Madeira[1,2]+Madeira[1,3]
+      
+      dosestotaisMadeira = c()
+      
+      for(i in 2:nrow(Madeira)) {
+        # for-loop over rows
+        if(Madeira[i,1]==Madeira[i-1,1]){
+          aux = aux + Madeira[i,2]+Madeira[i,3]
+          
+          if(i==nrow(Madeira)){
+            dosestotaisMadeira <- c(dosestotaisMadeira, aux)
+          }
+        }else{
+          dosestotaisMadeira <- c(dosestotaisMadeira, aux)
+          aux = Madeira[i,2]+Madeira[i,3]
+          if(i==nrow(Madeira)){
+            dosestotaisMadeira <- c(dosestotaisMadeira, aux)
+          }
+        }
+        
+      }
+      
+      dfMadeira <- data.frame(
+        datasMadeira,
+        dosestotaisMadeira
+      )
+      
+      #######################################
+      Norte <- loaded_Norte()
+      
+      Norte$FirstDoseRefused <- NULL
+      Norte$UnknownDose <- NULL
+      Norte$NumberDosesReceived <- NULL
+      Norte$Population <- NULL
+      Norte$ReportingCountry <- NULL
+      Norte$TargetGroup <- NULL
+      Norte$Denominator <- NULL
+      Norte$Region <- NULL
+      Norte$X <- NULL
+      datasNorte = unique(Norte["YearWeekISO"])
+      
+      aux=Norte[1,2]+Norte[1,3]
+      
+      dosestotaisNorte = c()
+      
+      for(i in 2:nrow(Norte)) {
+        # for-loop over rows
+        if(Norte[i,1]==Norte[i-1,1]){
+          aux = aux + Norte[i,2]+Norte[i,3]
+          
+          if(i==nrow(Norte)){
+            dosestotaisNorte <- c(dosestotaisNorte, aux)
+          }
+        }else{
+          dosestotaisNorte <- c(dosestotaisNorte, aux)
+          aux = Norte[i,2]+Norte[i,3]
+          if(i==nrow(Norte)){
+            dosestotaisNorte <- c(dosestotaisNorte, aux)
+          }
+        }
+        
+      }
+      
+      dfNorte <- data.frame(
+        datasNorte,
+        dosestotaisNorte
+      )
+      
+      
+      #######################################
+      
+      
+      df <- merge(dfAlentejo,dfAlgarve,by="YearWeekISO",all=TRUE)
+      df <- merge(df,dfAçores,by="YearWeekISO",all=TRUE)
+      df <- merge(df,dfNorte,by="YearWeekISO",all=TRUE)
+      df <- merge(df,dfCentro,by="YearWeekISO",all=TRUE)
+      df <- merge(df,dfLisboa,by="YearWeekISO",all=TRUE)
+      df <- merge(df,dfMadeira,by="YearWeekISO",all=TRUE)
+      
+      mydate <- as.Date("2020-12-24")
+      
+      for(i in 1:nrow(df)) {
+        # for-loop over rows
+        df[i,1]=i
+      }
+      
+      df      <- as.data.frame(t(df))
+      df$regiao <- rownames(df)
+      df      <- melt(df, id.vars=c("regiao"))
+      df$variable <- as.character(df$variable)
+      df$variable[df$variable=="V1"] <-"1"
+      df$variable[df$variable=="V2"] <-"2"
+      df$variable[df$variable=="V3"] <-"3"
+      df$variable[df$variable=="V4"] <-"4"
+      df$variable[df$variable=="V5"] <-"5"
+      df$variable[df$variable=="V6"] <-"6"
+      df$variable[df$variable=="V7"] <-"7"
+      df$variable[df$variable=="V8"] <-"8"
+      df$variable[df$variable=="V9"] <-"9"
+      df$variable[df$variable=="V10"] <-"10"
+      df$variable[df$variable=="V11"] <-"11"
+      df$variable[df$variable=="V12"] <-"12"
+      df$variable[df$variable=="V13"] <-"13"
+      df$variable[df$variable=="V14"] <-"14"
+      df$variable[df$variable=="V15"] <-"15"
+      df$variable[df$variable=="V16"] <-"16"
+      df$variable[df$variable=="V17"] <-"17"
+      df$variable[df$variable=="V18"] <-"18"
+      df$variable[df$variable=="V19"] <-"19"
+      df$variable[df$variable=="V20"] <-"20"
+      df$variable <- as.numeric(df$variable)
+      
+      df$regiao <- as.character(df$regiao)
+      df$regiao[df$regiao=="dosestotaisAlentejo"] <-"Alentejo"
+      df$regiao[df$regiao=="dosestotaisAlgarve"] <-"Algarve"
+      df$regiao[df$regiao=="dosestotaisMadeira"] <-"Madeira"
+      df$regiao[df$regiao=="dosestotaisAçores"] <-"Açores"
+      df$regiao[df$regiao=="dosestotaisCentro"] <-"Centro"
+      df$regiao[df$regiao=="dosestotaisNorte"] <-"Norte"
+      df$regiao[df$regiao=="dosestotaisLisboa"] <-"Área metropolitana de Lisboa"
+      
+      ggplot(df, 
+             aes(x = regiao,
+                 y = value,
+                 label = '')) +
+        geom_point(stat = "identity", 
+                   size = 3) +
+        
+        geom_text(color = 'black',
+                  size = 3,
+                  nudge_x = 0.7) +
+        coord_flip() +
+        
+        theme(legend.position = "none",
+              axis.title = element_text(size = 20,
+                                        face = 'bold'),
+              plot.subtitle = element_text(size = 20,
+                                           face = "bold",
+                                           color = 'red'),
+              axis.text.y = element_text( vjust = 0.5,size=10),
+              axis.text = element_text(size = 5,
+                                       face = "bold"),
+              axis.text.x = element_text(angle = 90,size = 12,
+                                         face = "bold")) +
+        labs(x = 'Regiões',
+             y = 'Doses administradas',
+             title ='Evolução das doses administradas'
+        )
+      #+
+      # transition_time(variable) +
+      #ease_aes('cubic-in-out')
+      
+      
+    })
+    
+    #Pedir ajuda à professora. Se passar como x = as.Date(res$Datas) não me aparece a data como queremos, mas apenas os meses.
+    
+    
+  })
